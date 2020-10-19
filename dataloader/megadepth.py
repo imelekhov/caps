151dbc4b6cd64e01eba172b6dbbab33b47a3f3ee
Iaro <iaroslav.melekhov@aalto.fi>
2020-10-19 17:49:37 +0300

    The dataloader has been changed

--- dataloader/megadepth.py
+++ dataloader/megadepth.py
@@ -9,6 +9,7 @@ import utils
 import collections
 from tqdm import tqdm
 import dataloader.data_utils as data_utils
+from assets.stylization.stylizer import Stylizer
 
 
 rand = np.random.RandomState(234)
@@ -43,11 +44,12 @@ class MegaDepthLoader():
 class MegaDepth(Dataset):
     def __init__(self, args, phase):
         self.args = args
+        self.phase = phase
 
         self.n_imgs = {"train": 2000,
                        "test": 500}
+        self.stylizer = Stylizer(self.args.styledir) if self.args.use_stylization else None
 
-        self.phase = phase
         if self.phase == 'train':
             # augment during training
             self.transform = transforms.Compose([transforms.ToPILImage(),
@@ -153,8 +155,13 @@ class MegaDepth(Dataset):
         imf2 = self.imf2s[item]
         im1_meta = self.images[imf1]
         im2_meta = self.images[imf2]
+
         im1 = io.imread(imf1)
         im2 = io.imread(imf2)
+        if self.args.use_stylization:
+            if np.random.randint(2, size=1)[0]:
+                im2 = self.stylizer.forward(imf2)
+
         h, w = im1.shape[:2]
 
         intrinsic1 = self.get_intrinsics(im1_meta)
